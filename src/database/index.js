958bbbc8998bbe706c2e38bface730a164777b06
Roger <12768403+rogix@users.noreply.github.com>
2019-07-03 00:14:49 -0300

    chore: create models loader

---
+++ src/database/index.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+// arr com models da aplicacao
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    // this.connection: variavel esperada em models/User -> init()
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
