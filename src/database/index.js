21690a26ef6e1e9c05ba9ee1d2b04e36677ec572
Roger <12768403+rogix@users.noreply.github.com>
2019-08-02 21:37:59 -0300

    chores: add user avatar

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,12 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
 // arr com models da aplicacao
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -16,7 +17,9 @@ class Database {
     // this.connection: variavel esperada em models/User -> init()
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
