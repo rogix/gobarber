d3826e7fa49fe265b00ae1be5cfc8cc1401040c2
Roger <12768403+rogix@users.noreply.github.com>
2019-08-03 00:08:59 -0300

    chores: add service schedule

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -4,7 +4,7 @@ class Appointment extends Model {
   static init(sequelize) {
     super.init(
       {
-        data: Sequelize.DATE,
+        date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
       },
       {
