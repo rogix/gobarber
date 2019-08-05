266a81bdd2a53fa0deb42f812b4ef9c0d36d4268
Roger <12768403+rogix@users.noreply.github.com>
2019-08-05 20:57:44 -0300

    chores: add exception treatments

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -21,7 +21,7 @@ class AvailableConroller {
 
     const searchDate = Number(date);
 
-    const appointments = await Appointment.findAll({
+    const appointments = await Appointment.finddAll({
       where: {
         provider_id: req.params.providerId,
         canceled_at: null,
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://d6ac6637db8e44af98c1c68d64e20d96@sentry.io/1522303',
+};
