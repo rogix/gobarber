a92c7c19a00fac09bc3ff15cb4429d37c47eea66
Roger <12768403+rogix@users.noreply.github.com>
2019-08-05 14:41:22 -0300

    chores: sets up nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'cd21dd8d796ee2',
+    pass: '60fe9192f4ecce',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
