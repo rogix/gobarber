d38eda63623c216112099732a2aa5e6fa4ddc191
Roger <12768403+rogix@users.noreply.github.com>
2019-08-05 20:13:48 -0300

    chores: list available time for appointment

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableConroller from './app/controllers/AvailableController';
 
 import authMiddeware from './app/middlewares/auth';
 
@@ -23,6 +24,7 @@ routes.use(authMiddeware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableConroller.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
