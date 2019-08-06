cf2547a90da994ada548b855de7d6022841cc020
Roger <12768403+rogix@users.noreply.github.com>
2019-08-05 21:22:07 -0300

    chores: environment variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'cd21dd8d796ee2',
-    pass: '60fe9192f4ecce',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://d6ac6637db8e44af98c1c68d64e20d96@sentry.io/1522303',
+  dsn: process.env.SENTRY_DSN,
 };
